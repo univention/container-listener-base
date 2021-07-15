2ce42aab7213f9ac575efdff514ba22bf55acc95
Per Macha <macha@univention.de>
2021-07-16 00:00:00 +0200

    feat(ci): add ci-libs [iam-issues#150]

--- ci-publish-script.py
+++ ci-publish-script.py
@@ -5,119 +5,64 @@
 """Build script for gitlab-ci"""
 
 # included
-import logging
 import os
 import sys
-import time
 
 # third party
 import sh  # pylint: disable=import-error
 
+# internal imports
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+LIBS_DIR = os.path.join(BASE_DIR, 'lib')
+sys.path.insert(1, LIBS_DIR)
+
+import ci_docker  # noqa: E402,E501; pylint: disable=import-error,wrong-import-position
+from ci_log import (  # noqa: E402,E501; pylint: disable=import-error,wrong-import-position
+    log,
+)
+import ci_vars  # noqa: E402,E501; pylint: disable=import-error,wrong-import-position
+import ci_version  # noqa: E402,E501; pylint: disable=import-error,wrong-import-position
+
 # pylint: disable=not-callable
-sh2 = sh(_out='/dev/stdout', _err='/dev/stderr')
-
-DEFAULT_UPX_IMAGE_REGISTRY = 'artifacts.knut.univention.de/upx/'
-
-DEFAULT_CI_PIPELINE_ID = 'none'
-
-MINIMAL_DOCKER_VARS = ['DOCKER_HOST']
-
-ADDITIONAL_PULL_PUSH_VARS = (
-    'DBUS_SESSION_BUS_ADDRESS',
-    'PATH',
-)
-
-logging.Formatter.converter = time.gmtime
-logging.basicConfig(
-    format='%(asctime)s %(levelname)-8s %(message)s',
-    level=logging.DEBUG,
-    datefmt='%Y-%m-%dT%H:%M:%SZ',
-)
-
-
-class AppVersionNotFound(Exception):
-    """Raised if /version file could not be read"""
-
-
-def get_app_version(image_name, docker_env):
-    """Retrieves content of /version file from a container"""
-    logging.info('Retrieving /version from {}'.format(image_name))
-    result = sh2.docker.run(
-        '--rm',
-        '--entrypoint=/bin/cat',
-        image_name,
-        '/version',
-        _env=docker_env,
-        _out=None,
-    ).stdout
-    app_version = result.rstrip().decode('ascii')
-    if not app_version:
-        raise AppVersionNotFound
-    return app_version
-
-
-def add_and_push_tag(image_name, tag, docker_env, pull_push_env):
-    """Adds a tag to an image"""
-    logging.info('Adding tag {} to {}'.format(tag, image_name))
-    sh2.docker.tag(image_name, tag, _env=docker_env)
-    sh2.docker.push(tag, _env=pull_push_env)
-    logging.info('Done with this tag')
-
-
-def get_env_vars(var_names):
-    """Check for environmental variables and return them as a dict"""
-    env_vars = {}
-    for var_name in var_names:
-        if var_name in os.environ:
-            env_vars[var_name] = os.environ[var_name]
-    return env_vars
+sh_out = sh(_out='/dev/stdout', _err='/dev/stderr', _cwd=BASE_DIR)
 
 
 def main():
     """The main script builds, labels and pushes"""
 
-    docker_env = get_env_vars(MINIMAL_DOCKER_VARS)
+    envs = ci_vars.get_docker_envs(BASE_DIR, pull_push=True)
 
-    pull_push_env = get_env_vars(ADDITIONAL_PULL_PUSH_VARS)
-    pull_push_env.update(docker_env)
-
-    ci_pipeline_id = os.environ.get('CI_PIPELINE_ID', DEFAULT_CI_PIPELINE_ID)
+    ci_pipeline_id = os.environ.get(
+        'CI_PIPELINE_ID', ci_vars.DEFAULT_CI_PIPELINE_ID
+    )
 
     upx_image_registry = os.environ.get(
         'UPX_IMAGE_REGISTRY',
-        DEFAULT_UPX_IMAGE_REGISTRY,
+        ci_vars.DEFAULT_UPX_IMAGE_REGISTRY,
     )
 
     image_path = '{}container-listener-base/listener'.format(
         upx_image_registry
     )
-    build_path = '{}:build-{}'.format(image_path, ci_pipeline_id)
 
     try:
-        sh2.docker.pull(build_path, _env=pull_push_env)
-    except sh.ErrorReturnCode_1:  # pylint: disable=no-member
-        logging.error('dock pull failed')
-        return 1
-
-    try:
-        app_version = get_app_version(build_path, docker_env)
-    except AppVersionNotFound:
+        # push tags "latest" and "<version>"
+        ci_docker.pull_add_push_publish_version_tag(
+            image_path,
+            ci_pipeline_id,
+            envs['docker'],
+            envs['pull_push'],
+            image_path,
+        )
+    except ci_version.AppVersionNotFound:
+        log.error('app version not found')
         return 2
-
-    logging.debug('pull_push_env {}'.format(pull_push_env))
-
-    try:
-        sh2.docker.push(build_path, _env=pull_push_env)
-    except sh.ErrorReturnCode_1:  # pylint: disable=no-member
-        logging.error('dock push failed')
+    except ci_docker.DockerPullFailed:
+        log.error('docker pull failed')
+        return 1
+    except ci_docker.DockerPushFailed:
+        log.error('docker push failed')
         return 3
-
-    tag = '{}:latest'.format(image_path)
-    add_and_push_tag(build_path, tag, docker_env, pull_push_env)
-
-    clean_version = app_version.replace('~', '-')
-    tag = '{}:{}'.format(image_path, clean_version)
-    add_and_push_tag(build_path, tag, docker_env, pull_push_env)
 
     return 0
 
