bed39a8203df93f4292e8fa4d60b28ce72f4c1d6
Per Macha <macha@univention.de>
2023-07-04 00:00:00 +0200

    feat(docker): pre-define entrypoint env-vars

--- listener-base-entrypoint.sh
+++ listener-base-entrypoint.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 set -euxo pipefail
 
+DOMAIN_NAME="${DOMAIN_NAME:-univention-organization.intranet}"
+CA_CERT_FILE="${CA_CERT_FILE:-/dev/null}"
+CERT_PEM_FILE="${CERT_PEM_FILE:-/dev/null}"
+LDAP_HOST="${LDAP_HOST:-ldap.univention-organization.intranet}"
+LDAP_PORT="${LDAP_PORT:-389}"
+LDAP_BASE_DN="${LDAP_BASE_DN:-dc=univention-organization,dc=intranet}"
+LDAP_HOST_DN="${LDAP_HOST_DN:-cn=ucs-4711,cn=dc,cn=computers,dc=univention-organization,dc=intranet}"
+LDAP_BIND_DN="${LDAP_BIND_DN:-cn=admin,${LDAP_BASE_DN}}"
+DEBUG_LEVEL="${DEBUG_LEVEL:-5}"
+NOTIFIER_SERVER="${NOTIFIER_SERVER:-notifier.univention-organization.intranet}"
+LDAP_BIND_SECRET="${LDAP_BIND_SECRET:-/dev/null}"
 START_TLS="${START_TLS:-require}"
+TLS_REQCERT=${TLS_REQCERT:-demand}
 
 target_dir="/etc/univention/ssl/ucs-6045.${DOMAIN_NAME}"
 mkdir --parents "${target_dir}" "/etc/univention/ssl/ucsCA/"
@@ -31,7 +43,7 @@ cat <<EOF > /etc/ldap/ldap.conf
 # This file should be world readable but not world writable.
 
 TLS_CACERT /etc/univention/ssl/ucsCA/CAcert.pem
-TLS_REQCERT ${TLS_REQCERT:-demand}
+TLS_REQCERT ${TLS_REQCERT}
 
 URI ldap://${LDAP_HOST}:${LDAP_PORT}
 
